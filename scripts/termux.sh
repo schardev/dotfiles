6b98c39d9bdb870354272c17ec81dae9c7053627
Saurabh Charde <saurabhchardereal@gmail.com>
2022-06-14 15:16:55 +0530

    refactor(termux): make script as lib for granular control
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

---
+++ scripts/termux.sh
@@ -0,0 +1,68 @@
+#!/data/data/com.termux/files/usr/bin/bash
+#
+# SPDX-License-Identifier: MIT
+#
+# Copyright (C) 2020-2021 Saurabh Charde <saurabhchardereal@gmail.com>
+#
+
+CONFIG_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"/..
+source "$CONFIG_DIR"/dotfiles/zsh/functions/_utils
+source "$CONFIG_DIR"/scripts/git-setup.sh
+
+# Install packages
+termux_install_packages() {
+    # List of packages to install
+    local packages=(
+        clang
+        exa
+        gh
+        gnupg
+        jq
+        ncurses-utils
+        neovim
+        nodejs
+        openssh
+        sheldon
+        zoxide
+        zsh
+    )
+
+    # Update package lists and update preinstalled packages
+    pkg update -y && pkg upgrade -y
+
+    pr_info "Installing packages ..."
+    pkg install --no-install-recommends -y "${packages[@]}"
+}
+
+# Theme
+termux_setup_theme() {
+    # use Dracula color scheme & Hack font by default
+    pr_info "Setting up theme ..."
+    cp "$CONFIG_DIR"/etc/Dracula-termux.properties "$HOME"/.termux/colors.properties
+    cp "$CONFIG_DIR"/etc/Hack.ttf "$HOME"/.termux/font.ttf
+}
+
+# Setup dotfiles
+termux_setup_dotfiles() {
+    pr_info "Setting up dotfiles ..."
+
+    # Settings I use for moi setup
+    [[ ! -d ~/.termux ]] && mkdir ~/.termux
+    echo "extra-keys = [['ESC','SHIFT','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]" >~/.termux/termux.properties
+
+    # Make zsh follow $XDA_CONFIG_DIR
+    echo "export ZDOTDIR=~/.config/zsh" >"${HOME}"/.zshenv
+    mkdir "${HOME}"/{.config,.sheldon}
+    ln -sf "$CONFIG_DIR"/dotfiles/zsh "${HOME}"/.config/zsh
+    ln -sf "$CONFIG_DIR"/dotfiles/nvim "${HOME}"/.config/nvim
+    ln -sf "$CONFIG_DIR"/dotfiles/zsh/plugins.toml "${HOME}"/.sheldon
+}
+
+termux_setup_env() {
+    termux_install_packages
+    termux_setup_dotfiles
+    termux_setup_theme
+    git_setup
+    chsh -s zsh # Switch shell to zsh
+    pr_succ "Setup done!"
+}
