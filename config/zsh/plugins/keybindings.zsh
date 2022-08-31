5312a2846aa341b1aa700248b873bda2341cf9f1
Saurabh Charde <saurabhchardereal@gmail.com>
2022-08-31 18:17:47 +0530

    feat(zsh): map `ctrl + ←` to `backward-word`
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- config/zsh/plugins/keybindings.zsh
+++ config/zsh/plugins/keybindings.zsh
@@ -4,6 +4,7 @@ bindkey '^[[B' history-substring-search-down # Down arrow
 bindkey '^[[H' beginning-of-line # Home key
 bindkey '^[[F' end-of-line # End key
 bindkey '^[[1;5C' forward-word # Ctrl + Right arrow
+bindkey '^[[1;5D' backward-word # Ctrl + Left arrow
 
 if [ -z "$IS_TERMUX" ]; then
     setxkbmap -option "caps:escape" # Use caps lock as escape key
