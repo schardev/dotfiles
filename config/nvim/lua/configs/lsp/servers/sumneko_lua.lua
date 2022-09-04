58ae261f51b132ce47ea658fb399a9c0d560a87e
Saurabh Charde <saurabhchardereal@gmail.com>
2022-09-04 21:20:11 +0530

    refactor(nvim/lsp): use new option to disable default snippet config
    ref: https://github.com/folke/lua-dev.nvim/pull/47
    
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- config/nvim/lua/configs/lsp/servers/sumneko_lua.lua
+++ config/nvim/lua/configs/lsp/servers/sumneko_lua.lua
@@ -1,12 +1,9 @@
 local M = {}
 
-M = vim.tbl_deep_extend("force", require("lua-dev").setup({}), {
-    settings = {
-        Lua = {
-            completion = { callSnippet = "Disable" },
-            workspace = { maxPreload = nil },
-        },
-    },
-})
+M = vim.tbl_deep_extend(
+    "force",
+    require("lua-dev").setup({ snippet = false }),
+    {}
+)
 
 return M
