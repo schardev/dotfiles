b8b549d79ca2e78beaa98079750c294f4d6eaa78
Saurabh Charde <saurabhchardereal@gmail.com>
2022-11-05 10:51:22 +0530

    chore(nvim): update lsp capabilities
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- config/nvim/lua/configs/lsp/init.lua
+++ config/nvim/lua/configs/lsp/init.lua
@@ -41,9 +41,9 @@ handlers.setup()
 diagnostics.setup()
 
 -- Update capabilities
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
-capabilities.textDocument.completion.completionItem.snippetSupport = true
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+capabilities.textDocument.completion =
+    { completionItem = { snippetSupport = true } }
 
 -- Setup all listed servers
 for lsp, config in pairs(servers) do
