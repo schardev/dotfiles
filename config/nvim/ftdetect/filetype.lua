63917690e14cea1cc20e5666648c98d6e0e5a291
Saurabh Charde <saurabhchardereal@gmail.com>
2022-11-12 21:02:58 +0530

    chore(nvim): properly detect `tsconfig` filetype
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- config/nvim/ftdetect/filetype.lua
+++ config/nvim/ftdetect/filetype.lua
@@ -3,11 +3,11 @@ vim.filetype.add({
         [".eslintrc.json"] = "jsonc",
         ["coc-settings.json"] = "jsonc",
         ["keybindings.json"] = "jsonc",
-        ["tsconfig.json"] = "jsonc",
     },
     pattern = {
         ["*.cjsn"] = "jsonc",
         ["*.cjson"] = "jsonc",
         [".*/zsh/functions/.*"] = "zsh",
+        ["tsconfig.*.json"] = "jsonc",
     },
 })
