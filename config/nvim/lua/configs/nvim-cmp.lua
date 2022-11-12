8458a122d158f1a778971759e42fcf2bc53e7dc8
Saurabh Charde <saurabhchardereal@gmail.com>
2022-11-12 21:04:24 +0530

    feat(nvim): close completion menu after pressing `<C-Space>` again
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- config/nvim/lua/configs/nvim-cmp.lua
+++ config/nvim/lua/configs/nvim-cmp.lua
@@ -60,13 +60,16 @@ cmp.setup({
         -- completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
     },
-    -- view = {
-    --     entries = "native",
-    -- },
     mapping = cmp.mapping.preset.insert({
         ["<C-d>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-Space>"] = cmp.mapping(function()
+            if cmp.visible() then
+                cmp.close()
+            else
+                cmp.complete()
+            end
+        end),
         ["<CR>"] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Replace,
             -- select = true,
