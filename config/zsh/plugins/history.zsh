aa067bc2636f8515a259bc39c7735d9b61bebb3a
Saurabh Charde <saurabhchardereal@gmail.com>
2023-02-03 17:47:13 +0530

    feat(zsh): don't push command prefixed with space to history stack

--- config/zsh/plugins/history.zsh
+++ config/zsh/plugins/history.zsh
@@ -8,6 +8,7 @@ setopt hist_expire_dups_first   # Trim duplicate entries first
 setopt hist_find_no_dups        # Don't show repeated commands
 setopt hist_ignore_all_dups     # Removes older entry if it dublicates the current
 setopt hist_ignore_dups         # Ignore current command if previously entered
+setopt hist_ignore_space        # Ignore command if it starts with a space
 
 # Change highlighting for matched strings (need `zsh-history-substring-search` plugin)
 HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND="bg=green,fg=black,bold"
