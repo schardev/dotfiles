331c401d58183f25a26565222fa3357f9c6162b7
Saurabh Charde <saurabhchardereal@gmail.com>
2023-02-09 15:49:48 +0530

    chore(nvim): disable `emmet-vim` since it causing issues with spell

--- config/nvim/lua/plugins/emmet-vim.lua
+++ config/nvim/lua/plugins/emmet-vim.lua
@@ -10,6 +10,7 @@ return {
     "typescript",
     "typescriptreact",
   },
+  enabled = false, -- FIXME: disabled because it fucks up spell (ik, kinda weird)
   config = function()
     local mapper = require("core.utils").mapper_factory
     -- Wrap selected text in emmet abbrev
