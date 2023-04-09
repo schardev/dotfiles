f9f43451f9a765d0d975296de52ee708fbd2f331
Saurabh Charde <saurabhchardereal@gmail.com>
2023-04-09 15:56:07 +0530

    chore(nvim): revert "feat(nvim): enable `colorizer` for scss files"
    - it's buggy
    
    This reverts commit e835fb421823a11833aeb243aca7b4a1e00b718b.

--- config/nvim/lua/plugins/colorizer.lua
+++ config/nvim/lua/plugins/colorizer.lua
@@ -13,7 +13,6 @@ return {
       hsl_fn = true, -- CSS hsl() and hsla() functions
       css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
       css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-      sass = { enable = true, parsers = { "css" }, }
     },
   },
 }
