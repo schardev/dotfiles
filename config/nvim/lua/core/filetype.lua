5e7bd6b0415542fbc04741f81dd9fe8bae9467a5
Saurabh Charde <saurabhchardereal@gmail.com>
2023-04-23 22:37:07 +0530

    chore(nvim): update filetypes

--- config/nvim/lua/core/filetype.lua
+++ config/nvim/lua/core/filetype.lua
@@ -1,12 +1,15 @@
 vim.filetype.add({
+  extension = {
+    cjsn = "jsonc",
+    cjson = "jsonc",
+    mdx = "markdown",
+  },
   filename = {
     [".eslintrc.json"] = "jsonc",
     ["coc-settings.json"] = "jsonc",
     ["keybindings.json"] = "jsonc",
   },
   pattern = {
-    ["*.cjsn"] = "jsonc",
-    ["*.cjson"] = "jsonc",
     [".*/zsh/functions/.*"] = "zsh",
     ["tsconfig.*.json"] = "jsonc",
   },
