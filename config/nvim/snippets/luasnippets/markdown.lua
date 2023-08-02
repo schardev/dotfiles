82a8a61d3a0e0f55b1e9af49a9ee0475b1626839
Saurabh Charde <saurabhchardereal@gmail.com>
2023-08-02 19:51:52 +0530

    feat(nvim): update markdown snippets

--- config/nvim/snippets/luasnippets/markdown.lua
+++ config/nvim/snippets/luasnippets/markdown.lua
@@ -9,16 +9,19 @@ local template = [[
 local function fenced_code_block(lang, placeholder)
   return s(
     { trig = lang, name = lang:upper() .. " fenced code block" },
-    fmt(template, { t(lang), i(1, placeholder) })
+    fmt(template, { t(lang), insert_selected_text(1, placeholder) })
   )
 end
 
 return {
-  s({ trig = "code", name = "code block" }, fmt(template, { i(1), i(2) })),
-  fenced_code_block("html", ""),
-  fenced_code_block("css", ""),
-  fenced_code_block("bash", ""),
-  fenced_code_block("js", ""),
-  fenced_code_block("ts", ""),
-  fenced_code_block("tsx", ""),
+  s(
+    { trig = "code", name = "code block" },
+    fmt(template, { i(1), insert_selected_text(2) })
+  ),
+  fenced_code_block("html"),
+  fenced_code_block("css"),
+  fenced_code_block("bash"),
+  fenced_code_block("js"),
+  fenced_code_block("ts"),
+  fenced_code_block("tsx"),
 }
