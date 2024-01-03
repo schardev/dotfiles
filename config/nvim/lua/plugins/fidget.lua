12e8e26784cc9dcd64ba44cd57eee596e29299e9
Saurabh Charde <saurabhchardereal@gmail.com>
2024-01-03 19:12:29 +0530

    chore(nvim): update fidget config

--- config/nvim/lua/plugins/fidget.lua
+++ config/nvim/lua/plugins/fidget.lua
@@ -1,19 +1,12 @@
 return {
   "j-hui/fidget.nvim",
-  event = "LspAttach",
-  config = function()
-    vim.api.nvim_create_autocmd(
-      "VimLeavePre",
-      { command = [[silent! FidgetClose]] }
-    )
-
-    require("fidget").setup({
-      notification = {
-        override_vim_notify = true,
-        window = {
-          winblend = 25,
-        },
+  event = "VeryLazy",
+  opts = {
+    notification = {
+      override_vim_notify = true,
+      window = {
+        winblend = 25,
       },
-    })
-  end,
+    },
+  },
 }
