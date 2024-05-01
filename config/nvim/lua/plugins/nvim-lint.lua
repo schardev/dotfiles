69df02accb27296b9dede39df2c22b79934c6e10
Saurabh Charde <saurabhchardereal@gmail.com>
2024-05-01 14:56:36 +0530

    chore(nvim): update nvim lint

--- config/nvim/lua/plugins/nvim-lint.lua
+++ config/nvim/lua/plugins/nvim-lint.lua
@@ -1,11 +1,13 @@
 ---@type LazySpec
 return {
   "mfussenegger/nvim-lint",
-  event = { "BufReadPre", "BufNewFile" },
+  event = { "BufReadPost", "BufNewFile" },
   dependencies = { "mason.nvim" },
   config = function()
+    local lint = require("lint")
+
     vim.api.nvim_create_user_command("LintInfo", function()
-      local linters = require("lint").get_running()
+      local linters = lint.get_running()
       if #linters == 0 then
         return vim.notify("No linters running")
       end
@@ -17,12 +19,12 @@ return {
       {
         group = vim.api.nvim_create_augroup("UserLint", { clear = true }),
         callback = function()
-          require("lint").try_lint()
+          lint.try_lint()
         end,
       }
     )
 
-    require("lint").linters_by_ft = {
+    lint.linters_by_ft = {
       bash = { "shellcheck" },
       sh = { "shellcheck" },
       zsh = { "zsh" },
@@ -30,6 +32,18 @@ return {
       -- javascriptreact = { "eslint_d" },
       -- typescript = { "eslint_d" },
       -- typescriptreact = { "eslint_d" },
+    }
+
+    lint.linters.shellcheck.args = {
+      "-s",
+      "bash",
+      "-e", -- Start ignoring below rules:
+      -- "SC1090" - Can't follow non-constant source. Use a directive to specify location
+      -- "SC1091" - Almost the same reason as above
+      "SC1090,SC1091",
+      "--format",
+      "json",
+      "-",
     }
   end,
 }
