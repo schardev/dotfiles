253f010367d8c3915097e9a6128be0c457606749
Saurabh Charde <saurabhchardereal@gmail.com>
2024-09-05 15:44:02 +0530

    feat(nvim): add typescript type surround motion

--- config/nvim/after/ftplugin/typescript.lua
+++ config/nvim/after/ftplugin/typescript.lua
@@ -1,2 +1,38 @@
 -- Source javascript ftplugin
 vim.cmd("runtime! ftplugin/javascript.lua")
+
+-- Surround
+local installed, surround = pcall(require, "nvim-surround")
+
+if not installed then
+  return
+end
+
+local config = require("nvim-surround.config")
+
+surround.buffer_setup({
+  surrounds = {
+    -- https://github.com/kylechui/nvim-surround/discussions/53
+    ["g"] = {
+      add = function()
+        local result = config.get_input("Enter the generic name: ")
+        if result then
+          return { { result .. "<" }, { ">" } }
+        end
+      end,
+      find = function()
+        return config.get_selection({ node = "generic_type" })
+      end,
+      delete = "^(.-<)().-(>)()$",
+      change = {
+        target = "^(.-<)().-(>)()$",
+        replacement = function()
+          local result = config.get_input("Enter the generic name: ")
+          if result then
+            return { { result .. "<" }, { ">" } }
+          end
+        end,
+      },
+    },
+  },
+})
