c17d019694b264229a5a19c20d35f34d74b0eee9
Saurabh Charde <saurabhchardereal@gmail.com>
2024-09-19 20:30:31 +0530

    feat(nvim): update snippets

--- config/nvim/snippets/luasnippets/javascript.lua
+++ config/nvim/snippets/luasnippets/javascript.lua
@@ -40,4 +40,42 @@ return {
       i(1, "specifier"),
     })
   ),
+
+  -- useeffect
+  s(
+    { trig = "rue", name = "React useEffect" },
+    fmt(
+      [[
+  useEffect(() => {{
+    {}
+    {}
+  }}{})
+  ]],
+      {
+        i(1),
+        c(2, {
+          i(1),
+          sn(nil, {
+            t({ "return () => {", "" }),
+            i(1),
+            t({ "", "\t}" }),
+          }),
+        }),
+        c(3, {
+          i(1),
+          sn(nil, { t(", ["), i(1), t("]") }),
+        }),
+      }
+    )
+  ),
+
+  -- usestate
+  s(
+    { trig = "rus", name = "React useState" },
+    fmt("const [{}, set{}] = useState({});", {
+      i(1, "state"),
+      l(l._1:gsub("^%l", string.upper), 1),
+      i(2),
+    })
+  ),
 }
