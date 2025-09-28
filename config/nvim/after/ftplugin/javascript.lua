0a125458ede21031733f97b1ce23178d0ac25543
Saurabh Charde <saurabhchardereal@gmail.com>
2025-09-28 15:03:52 +0530

    feat(nvim): add template string surround motion

--- config/nvim/after/ftplugin/javascript.lua
+++ config/nvim/after/ftplugin/javascript.lua
@@ -6,3 +6,36 @@ vim.keymap.set("n", "<Leader>no", function()
   vim.cmd("botright terminal node")
   vim.cmd("wincmd J | startinsert")
 end, { desc = "Open node repl in a termial split below" })
+
+-- Surround
+local installed, surround = pcall(require, "nvim-surround")
+
+if not installed then
+  return
+end
+
+surround.buffer_setup({
+  surrounds = {
+    ["s"] = {
+      add = function()
+        local ts_utils = require("nvim-treesitter.ts_utils")
+        local cur = ts_utils.get_node_at_cursor(0, true)
+
+        if not cur then
+          return
+        end
+
+        local cur_type = cur:type()
+        local interpolation_surround = { { "${" }, { "}" } }
+        if cur and (cur_type == "string" or cur_type == "string_fragment") then
+          vim.cmd.normal("csq`")
+          return interpolation_surround
+        elseif cur and cur_type == "template_string" then
+          return interpolation_surround
+        else
+          return { { "`${" }, { "}`" } }
+        end
+      end,
+    },
+  },
+})
